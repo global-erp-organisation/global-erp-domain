12bb79abcd3fb9ce9f3a8c1f96fb68b93e68c72d
Martin Mbossi <Martin Mbossi>
2015-12-27 02:36:29 -0500

    update the config file

--- src/main/java/com/camlait/global/erp/domain/config/GlobalAppConstants.java
+++ src/main/java/com/camlait/global/erp/domain/config/GlobalAppConstants.java
@@ -6,35 +6,35 @@ public class GlobalAppConstants {
 
 	}
 
-	public final static String DAO_BASE_PACKAGE = "com.camlait.global.erp.dao";
-	public final static String DOMAIN_BASE_PACKAGE = "com.camlait.global.erp.domain";
-	public final static String SERVICE_BASE_PACKAGE = "com.camlait.global.erp.service";
+	public final static String ROOT_PACKAGE = "com.camlait.global.erp";
+
+	public final static String DAO_BASE_PACKAGE = ROOT_PACKAGE + ".dao";
+	public final static String DOMAIN_BASE_PACKAGE = ROOT_PACKAGE + ".domain";
 
 	// Domain base packages
-	public final static String PRODUCT_DOMAIN_BASE_PACKAGE = "com.camlait.global.erp.domain.produit";
-	public final static String DOCUMENT_DOMAIN_BASE_PACKAGE = "com.camlait.global.erp.domain.document";
-	public final static String AUTH_DOMAIN_BASE_PACKAGE = "com.camlait.global.erp.domain.auth";
-	public final static String STORE_DOMAIN_BASE_PACKAGE = "com.camlait.global.erp.domain.entrepot";
-	public final static String IMMO_DOMAIN_BASE_PACKAGE = "com.camlait.global.erp.domain.immobilisation";
-	public final static String INVENTORY_DOMAIN_BASE_PACKAGE = "com.camlait.global.erp.domain.inventaire";
-	public final static String LOCAL_DOMAIN_BASE_PACKAGE = "com.camlait.global.erp.domain.localisation";
-	public final static String OPERATION_DOMAIN_BASE_PACKAGE = "com.camlait.global.erp.domain.operation";
-	public final static String PARTNER_DOMAIN_BASE_PACKAGE = "com.camlait.global.erp.domain.partenaire";
-	public final static String BMQ_DOMAIN_BASE_PACKAGE = "com.camlait.global.erp.domain.bmq";
+	public final static String PRODUCT_DOMAIN_BASE_PACKAGE = ROOT_PACKAGE + DOMAIN_BASE_PACKAGE + ".produit";
+	public final static String DOCUMENT_DOMAIN_BASE_PACKAGE = ROOT_PACKAGE + DOMAIN_BASE_PACKAGE + ".document";
+	public final static String AUTH_DOMAIN_BASE_PACKAGE = ROOT_PACKAGE + DOMAIN_BASE_PACKAGE + ".auth";
+	public final static String STORE_DOMAIN_BASE_PACKAGE = ROOT_PACKAGE + DOMAIN_BASE_PACKAGE + ".entrepot";
+	public final static String IMMO_DOMAIN_BASE_PACKAGE = ROOT_PACKAGE + DOMAIN_BASE_PACKAGE + ".immobilisation";
+	public final static String INVENTORY_DOMAIN_BASE_PACKAGE = ROOT_PACKAGE + DOMAIN_BASE_PACKAGE + ".inventaire";
+	public final static String LOCAL_DOMAIN_BASE_PACKAGE = ROOT_PACKAGE + DOMAIN_BASE_PACKAGE + ".localisation";
+	public final static String OPERATION_DOMAIN_BASE_PACKAGE = ROOT_PACKAGE + DOMAIN_BASE_PACKAGE + ".operation";
+	public final static String PARTNER_DOMAIN_BASE_PACKAGE = ROOT_PACKAGE + DOMAIN_BASE_PACKAGE + ".partenaire";
+	public final static String BMQ_DOMAIN_BASE_PACKAGE = ROOT_PACKAGE + DOMAIN_BASE_PACKAGE + ".bmq";
 
 	// Repository base packages
-	public final static String PRODUCT_DAO_BASE_PACKAGE = "com.camlait.global.erp.dao.produit";
-	public final static String DOCUMENT_DAO_BASE_PACKAGE = "com.camlait.global.erp.dao.document";
-	public final static String AUTH_DAO_BASE_PACKAGE = "com.camlait.global.erp.dao.auth";
-	public final static String STORE_DAO_BASE_PACKAGE = "com.camlait.global.erp.dao.entrepot";
-	public final static String IMMO_DAO_BASE_PACKAGE = "com.camlait.global.erp.dao.immobilisation";
-	public final static String INVENTORY_DAO_BASE_PACKAGE = "com.camlait.global.erp.dao.inventaire";
-	public final static String LOCAL_DAO_BASE_PACKAGE = "com.camlait.global.erp.dao.localisation";
-	public final static String OPERATION_DAO_BASE_PACKAGE = "com.camlait.global.erp.dao.operation";
-	public final static String PARTNER_DAO_BASE_PACKAGE = "com.camlait.global.erp.dao.partenaire";
-	public final static String BMQ_DAO_BASE_PACKAGE = "com.camlait.global.erp.dao.bmq";
+	public final static String PRODUCT_DAO_BASE_PACKAGE = ROOT_PACKAGE + DAO_BASE_PACKAGE + ".produit";
+	public final static String DOCUMENT_DAO_BASE_PACKAGE = ROOT_PACKAGE + DAO_BASE_PACKAGE + ".document";
+	public final static String AUTH_DAO_BASE_PACKAGE = ROOT_PACKAGE + DAO_BASE_PACKAGE + ".auth";
+	public final static String STORE_DAO_BASE_PACKAGE = ROOT_PACKAGE + DAO_BASE_PACKAGE + ".entrepot";
+	public final static String IMMO_DAO_BASE_PACKAGE = ROOT_PACKAGE + DAO_BASE_PACKAGE + ".immobilisation";
+	public final static String INVENTORY_DAO_BASE_PACKAGE = ROOT_PACKAGE + DAO_BASE_PACKAGE + ".inventaire";
+	public final static String LOCAL_DAO_BASE_PACKAGE = ROOT_PACKAGE + DAO_BASE_PACKAGE + ".localisation";
+	public final static String OPERATION_DAO_BASE_PACKAGE = ROOT_PACKAGE + DAO_BASE_PACKAGE + ".operation";
+	public final static String PARTNER_DAO_BASE_PACKAGE = ROOT_PACKAGE + DAO_BASE_PACKAGE + ".partenaire";
+	public final static String BMQ_DAO_BASE_PACKAGE = ROOT_PACKAGE + DAO_BASE_PACKAGE + ".bmq";
 
-	
 	public static String buildNotFingMessage(Class<?> objectType, Object value) {
 		return "L'objet de type " + objectType.getCanonicalName() + " ayant l'identifiant " + String.valueOf(value)
 				+ " n'existe pas";
