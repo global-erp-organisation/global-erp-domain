f992eacf2f2276a1a1a4620a3d37bdfb809d8ea8
martin <a-msigne@expedia.com>
2016-01-05 17:02:30 -0500

    add jackson references

--- src/main/java/com/camlait/global/erp/domain/organisation/Centre.java
+++ src/main/java/com/camlait/global/erp/domain/organisation/Centre.java
@@ -3,6 +3,7 @@ package com.camlait.global.erp.domain.organisation;
 import java.util.Collection;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 
 import com.camlait.global.erp.domain.enumeration.AutreEnum;
@@ -10,7 +11,7 @@ import com.camlait.global.erp.domain.enumeration.AutreEnum;
 @Entity
 public class Centre extends Localisation {
 
-    @OneToMany(mappedBy = "centre")
+    @OneToMany(mappedBy = "centre", fetch=FetchType.LAZY)
     private Collection<Region> regions;
 
     public Collection<Region> getRegions() {
