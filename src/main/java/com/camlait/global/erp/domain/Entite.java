c89ab36c44f5291ae20036258ded131b920b9ae8
msigne <a-msigne@expedia.com>
2017-01-17 07:43:20 -0500

    code refactoring

--- src/main/java/com/camlait/global/erp/domain/Entite.java
+++ src/main/java/com/camlait/global/erp/domain/Entite.java
@@ -5,20 +5,14 @@ import java.io.Serializable;
 import com.camlait.global.erp.domain.util.MergeBeanUtilsBean;
 
 @SuppressWarnings("serial")
-public abstract class Entite implements Serializable{
-    private final MergeBeanUtilsBean bean = new MergeBeanUtilsBean();
-
-    /**
-     * Merge the current entity with the given one.
-     * 
-     * @param from
-     * @return
-     */
-    public Entite merge(Entite from) {
-        try {
-            return bean.merge(from, this);
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage(), e);
-        }
-    }
+public abstract class Entite implements Serializable {
+	/**
+	 * Merge the current entity with the given one.
+	 * 
+	 * @param from
+	 * @return
+	 */
+	public Entite merge(Entite from) {
+		return MergeBeanUtilsBean.mergeDefault(from, this);
+	}
 }
