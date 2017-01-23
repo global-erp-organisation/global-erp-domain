01113d70df9ca0894e8463cff4c99a77d33967ae
msigne <a-msigne@expedia.com>
2017-01-22 20:35:28 -0500

    code refactoring

--- src/main/java/com/camlait/global/erp/domain/Entite.java
+++ src/main/java/com/camlait/global/erp/domain/Entite.java
@@ -1,6 +1,7 @@
 package com.camlait.global.erp.domain;
 
 import java.io.Serializable;
+import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.stream.Stream;
 
@@ -9,51 +10,59 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 import org.hibernate.Hibernate;
-import static org.apache.commons.lang.reflect.FieldUtils.readField;
 
-import com.camlait.global.erp.domain.exception.LazyInitException;
 import com.camlait.global.erp.domain.util.MergeBeanUtilsBean;
 
+import lombok.NonNull;
+
 /**
- * Entity bas class
+ * Entity base class
  * 
  * @author Martin Blaise Signe
  */
 @SuppressWarnings("serial")
 public abstract class Entite implements Serializable {
-    /**
-     * Merge the current entity with the given one.
-     * 
-     * @param from
-     * @return
-     */
-    @SuppressWarnings("unchecked")
-    public <T> T merge(T from) {
-        return (T) MergeBeanUtilsBean.mergeDefault(from, this);
-    }
+	/**
+	 * Merge the current entity with the given one.
+	 * 
+	 * @param from
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public <T> T merge(@NonNull T from) {
+		return (T) MergeBeanUtilsBean.mergeDefault(from, this);
+	}
 
-    /**
-     * Help to perform all the post constructor operations.
-     */
-    @PostConstruct
-    public abstract void postConstructOperation();
+	/**
+	 * Help to perform all the post constructor operations.
+	 */
+	@PostConstruct
+	public abstract void postConstructOperation();
 
-    /**
-     * Scan the current class in order to find all collection that need to lazy initialize.
-     * 
-     * @return The Object after lazy initialized collections.
-     */
-    @SuppressWarnings("unchecked")
-    public <T> T lazyInit() {
-        Stream.of(this.getClass().getFields()).filter(f -> {
-            return (f.getAnnotation(ManyToMany.class) != null) || (f.getAnnotation(OneToMany.class) != null) && (f.getType().isInstance(Collection.class));
-        }).forEach(f -> {
-            try {
-                Hibernate.initialize(readField(this, f.getName(), true));
-            } catch (Exception e) {
-                throw new LazyInitException(e.getMessage(), e);
-            }
-        });
-        return (T) this;
-    }
+	/**
+	 * Verify if the given class is an instance of the current class.
+	 * 
+	 * @param clazz
+	 * @return
+	 */
+	public <T> Boolean instanceOf(@NonNull Class<T> clazz) {
+		return this.getClass().getName().equals(clazz.getName());
+	}
+
+	/**
+	 * Scan the current class in order to find all collection that need to lazy
+	 * initialize.
+	 * 
+	 * @return The Object after lazy initialized collections.
+	 */
+	@SuppressWarnings("unchecked")
+	public <T extends Entite> T lazyInit() {
+		Stream.of(this.getClass().getFields()).filter(this::canBeLazyInit).forEach(f -> Hibernate.initialize(f));
+		return (T) this;
+	}
+
+	private Boolean canBeLazyInit(Field f) {
+		return (f.getAnnotation(ManyToMany.class) != null)
+				|| (f.getAnnotation(OneToMany.class) != null) && (f.getType().isInstance(Collection.class));
+	}
 }
