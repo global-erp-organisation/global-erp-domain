2960ef6029d7028485d05c17a04bc17d5f9855fc
msigne <a-msigne@expedia.com>
2017-01-23 11:54:12 -0500

    code refactoring

--- src/main/java/com/camlait/global/erp/domain/Entite.java
+++ src/main/java/com/camlait/global/erp/domain/Entite.java
@@ -1,5 +1,6 @@
 package com.camlait.global.erp.domain;
 
+import static org.apache.commons.lang.reflect.FieldUtils.readField;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.util.Collection;
@@ -8,9 +9,9 @@ import java.util.stream.Stream;
 import javax.annotation.PostConstruct;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
-
 import org.hibernate.Hibernate;
 
+import com.camlait.global.erp.domain.exception.LazyInitException;
 import com.camlait.global.erp.domain.util.MergeBeanUtilsBean;
 
 import lombok.NonNull;
@@ -40,12 +41,12 @@ public abstract class Entite implements Serializable {
 	public abstract void postConstructOperation();
 
 	/**
-	 * Verify if the given class is an instance of the current class.
+	 * Verify if the given class is the same type as the current class.
 	 * 
 	 * @param clazz
 	 * @return
 	 */
-	public <T> Boolean instanceOf(@NonNull Class<T> clazz) {
+	public <T> Boolean isTypeOf(@NonNull Class<T> clazz) {
 		return this.getClass().getName().equals(clazz.getName());
 	}
 
@@ -57,12 +58,21 @@ public abstract class Entite implements Serializable {
 	 */
 	@SuppressWarnings("unchecked")
 	public <T extends Entite> T lazyInit() {
-		Stream.of(this.getClass().getFields()).filter(this::canBeLazyInit).forEach(f -> Hibernate.initialize(f));
+		Stream.of(this.getClass().getFields()).filter(this::canBeLazyInit)
+				.forEach(f -> Hibernate.initialize(getFieldValue(f)));
 		return (T) this;
 	}
 
 	private Boolean canBeLazyInit(Field f) {
 		return (f.getAnnotation(ManyToMany.class) != null)
 				|| (f.getAnnotation(OneToMany.class) != null) && (f.getType().isInstance(Collection.class));
+	}
+
+	private Object getFieldValue(Field f) {
+		try {
+			return readField(this, f.getName(), true);
+		} catch (Exception e) {
+			throw new LazyInitException("Unable to get he field value. Field name: " + f.getName());
+		}
 	}
 }
