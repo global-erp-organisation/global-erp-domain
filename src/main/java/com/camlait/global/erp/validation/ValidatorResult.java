6da479f0aee6a9042187900fbd0696884989f92b
mbsig <mbsig@192.168.2.18>
2017-05-22 11:50:29 -0400

    Update the validator model

--- src/main/java/com/camlait/global/erp/validation/ValidatorResult.java
+++ src/main/java/com/camlait/global/erp/validation/ValidatorResult.java
@@ -6,6 +6,16 @@ import com.google.common.collect.Lists;
 
 import lombok.Data;
 
+/**
+ * Encapsulate the validation result
+ * <ul>
+ * <li>errors: List of string that represent the error messages after the validation</li>
+ * <li>result: Generic type that represent a result to return after the validation.
+ * </ul>
+ * 
+ * @author Martin Blaise Signe.
+ * @param <R> type of the object to return after the validation.
+ */
 @Data
 public class ValidatorResult<R> {
     private List<String> errors = Lists.newArrayList();
